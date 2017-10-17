95600afed9749ef5f67bf2f935bd5e5a876c4e38
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2017-10-17 19:41:25 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <md-toolbar id="toolbar" class="md-dense">
+      <h2 class="md-title" style="flex: 1">GW Marmos</h2>
+    </md-toolbar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#toolbar {
+  margin-bottom: 40px;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<template>
+  <md-layout md-align="center">
+    
+    <md-layout md-flex="25">
+      <md-card>
+        <md-card-media-cover md-text-scrim>
+          <md-card-media>
+            <img src="../assets/card-sky.jpg" alt="Skyscraper">
+          </md-card-media>
+
+          <md-card-area>
+            <md-card-header>
+              <div class="md-title">Title goes here</div>
+              <div class="md-subhead">Subtitle here</div>
+            </md-card-header>
+
+            <md-card-actions>
+              <md-button>Action</md-button>
+              <md-button>Action</md-button>
+            </md-card-actions>
+          </md-card-area>
+        </md-card-media-cover>
+      </md-card>
+    </md-layout>
+
+   
+  </md-layout>
+
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      projects: [],
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
